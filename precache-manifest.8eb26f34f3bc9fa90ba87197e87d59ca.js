1b69587572e200204edb4950a4fba9b8da7e6dc7
Eronbello <eron.bello@gmail.com>
2020-04-19 00:15:07 -0300

    Updates

---
+++ precache-manifest.8eb26f34f3bc9fa90ba87197e87d59ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42d59ee5edcb44cdf5c3f07ee916f18",
+    "url": "http://eronbello.github.io/find-issues/index.html"
+  },
+  {
+    "revision": "556ecb22f4d5d621e2e8",
+    "url": "http://eronbello.github.io/find-issues/static/js/2.78360cd6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "http://eronbello.github.io/find-issues/static/js/2.78360cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28a4a0aa05c7cd8be7f5",
+    "url": "http://eronbello.github.io/find-issues/static/js/main.d24b15ae.chunk.js"
+  },
+  {
+    "revision": "a404ab3237ef7246dad1",
+    "url": "http://eronbello.github.io/find-issues/static/js/runtime-main.979384f9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bf0291ccb6233c97ae0ac883742ae6d7.js"
+  "http://eronbello.github.io/find-issues/precache-manifest.8eb26f34f3bc9fa90ba87197e87d59ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://eronbello.github.io/find-issues/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
