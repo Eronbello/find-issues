4842f5471fc1e39389b5ffc14ea52fb59fa3367c
Eronbello <eron.bello@gmail.com>
2020-04-18 23:54:16 -0300

    Updates

---
+++ precache-manifest.6ec8becacc891f217f8ec07bb3ed167f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2261e3d02891da40627d4b2b9ed69a1e",
+    "url": "/find-issues/index.html"
+  },
+  {
+    "revision": "5b39f611d116c3fce5f3",
+    "url": "/find-issues/static/js/2.393366ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/find-issues/static/js/2.393366ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79702a5dcacea2eac7bb",
+    "url": "/find-issues/static/js/main.e5783fbd.chunk.js"
+  },
+  {
+    "revision": "5162d6c5cd5e3c7e50c1",
+    "url": "/find-issues/static/js/runtime-main.f3a07176.js"
+  }
+]);
